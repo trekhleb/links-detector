65afaab1cc7d07b1a644a5aa035459331c6fce97
Oleksii Trekhleb <trehleb@gmail.com>
2020-09-18 07:20:03 +0200

    Do smth.

---
+++ src/components/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Template from './shared/Template';
+
+function App(): React.ReactElement {
+  return (
+    <Template>
+      Hello
+    </Template>
+  );
+}
+
+export default App;
