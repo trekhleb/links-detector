c1a4ff37c86a1ec06ba8a893486aecb9d131b38f
Oleksii Trekhleb <trehleb@gmail.com>
2020-09-18 18:23:39 +0200

    Do smth.

---
+++ src/components/screens/HomeScreen.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function HomeScreen(): React.ReactElement {
+  return (
+    <div>
+      Home Screen
+    </div>
+  );
+}
+
+export default HomeScreen;
---
+++ src/components/screens/LiveScannerScreen.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function LiveScannerScreen(): React.ReactElement {
+  return (
+    <div>
+      Live Scanner Screen
+    </div>
+  );
+}
+
+export default LiveScannerScreen;
