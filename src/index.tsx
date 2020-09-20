48142aae6588ca370032d9e200092bc326d7152c
Oleksii Trekhleb <trehleb@gmail.com>
2020-09-20 19:51:49 +0200

    Do smth.

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import './index.css';
+import './styles/tailwind.css';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 
