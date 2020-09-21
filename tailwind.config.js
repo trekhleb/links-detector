54799505ed5958504b9a8062a5e424b6d3a81e3f
Oleksii Trekhleb <trehleb@gmail.com>
2020-09-21 08:18:55 +0200

    Do smth.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,27 +1,30 @@
 // @see: https://tailwindcss.com/docs/configuration/
 // @see: https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
+
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+const fontFamily = defaultTheme.fontFamily;
+fontFamily['sans'] = [
+  'Roboto',
+  'system-ui',
+  '-apple-system',
+  'BlinkMacSystemFont',
+  '"Segoe UI"',
+  '"Helvetica Neue"',
+  'Arial',
+  '"Noto Sans"',
+  'sans-serif',
+  '"Apple Color Emoji"',
+  '"Segoe UI Emoji"',
+  '"Segoe UI Symbol"',
+  '"Noto Color Emoji"',
+];
+
 module.exports = {
   purge: [],
   theme: {
-    extend: {
-      fontFamily: {
-        roboto: [
-          'Roboto',
-          'system-ui',
-          '-apple-system',
-          'BlinkMacSystemFont',
-          '"Segoe UI"',
-          '"Helvetica Neue"',
-          'Arial',
-          '"Noto Sans"',
-          'sans-serif',
-          '"Apple Color Emoji"',
-          '"Segoe UI Emoji"',
-          '"Segoe UI Symbol"',
-          '"Noto Color Emoji"',
-        ],
-      },
-    },
+    fontFamily: fontFamily,
+    extend: {},
   },
   variants: {
     margin: ['responsive', 'last'],
