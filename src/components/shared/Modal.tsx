d174dfccdb7d9a2468fa846ac7b4569926e24d4c
Oleksii Trekhleb <trehleb@gmail.com>
2020-09-22 18:29:38 +0200

    Do smth.

--- src/components/shared/Modal.tsx
+++ src/components/shared/Modal.tsx
@@ -1,17 +1,19 @@
 import React from 'react';
+import { Helmet } from 'react-helmet';
 
 type ModalProps = {
   children: React.ReactNode,
+  bgColorClass?: string,
 };
 
 function Modal(props: ModalProps): React.ReactElement {
-  const { children } = props;
+  const { children, bgColorClass = 'bg-black' } = props;
 
-  const bgColor: string = 'bg-black';
-  const modalContainerClasses = `${bgColor} absolute left-0 top-0 w-full h-full overflow-hidden flex items-center justify-center flex-col`;
+  const modalContainerClasses = `${bgColorClass} absolute left-0 top-0 w-full h-full overflow-hidden flex items-center justify-center flex-col`;
 
   return (
     <div className={modalContainerClasses}>
+      <Helmet bodyAttributes={{ class: `${bgColorClass}` }} />
       {children}
     </div>
   );
