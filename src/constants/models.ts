3421339381a4ca3b22ee37f92d92306ecad14dd3
Oleksii Trekhleb <trehleb@gmail.com>
2020-09-24 09:34:34 +0200

    Do smth.

--- src/constants/models.ts
+++ src/constants/models.ts
@@ -1,3 +1,2 @@
-// export const MODELS_BASE_URL = 'https://localhost:3000';
-export const MODELS_BASE_URL = process.env.PUBLIC_URL;
-export const LINKS_DETECTOR_MODEL_URL = `${MODELS_BASE_URL}/static/models/links_detector/model.json`;
+export const MODELS_BASE_URL = '';
+export const LINKS_DETECTOR_MODEL_URL = `${MODELS_BASE_URL}/models/links_detector/model.json`;
