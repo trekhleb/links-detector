7c23929b66640647dbcb5c1e172c3bb1a4982974
Oleksii Trekhleb <trehleb@gmail.com>
2020-09-26 08:58:12 +0200

    Do smth.

--- src/components/elements/LiveDetector.tsx
+++ src/components/elements/LiveDetector.tsx
@@ -9,6 +9,13 @@ import Notification, { NotificationLevel } from '../shared/Notification';
 import useLogger from '../../hooks/useLogger';
 import ProgressBar from '../shared/ProgressBar';
 
+type ModelPredictions = {
+  detectionsNum: number,
+  detectionScores: number[],
+  detectionClasses: number[],
+  detectionBoxes: number[][],
+};
+
 function LiveDetector(): React.ReactElement | null {
   const logger = useLogger({ context: 'LiveDetector' });
   const windowSize = useWindowSize();
@@ -43,22 +50,20 @@ function LiveDetector(): React.ReactElement | null {
 
   const videoSize: number = Math.min(windowSize.width, windowSize.height);
 
-  const executeModel = async (video: HTMLVideoElement): Promise<null> => {
+  const executeModel = async (video: HTMLVideoElement): Promise<ModelPredictions | null> => {
     if (!model || !video) {
       logger.logError('executeModel: model or video is undefined');
       return null;
     }
     const inputTensor: tf.Tensor3D = tf.browser.fromPixels(video).expandDims(0);
 
-    let result: tf.Tensor | tf.Tensor[];
+    let results: tf.Tensor | tf.Tensor[] | null = null;
 
     try {
+      results = await model.executeAsync(inputTensor);
       logger.logDebug('executeModel: executing', {
         inputTensorShape: inputTensor.shape,
-      });
-      result = await model.executeAsync(inputTensor);
-      logger.logDebug('executeModel: executed', {
-        result,
+        results,
       });
     } catch (e) {
       const errorMessage = (e && e.message) || 'Cannot execute the model';
@@ -66,11 +71,56 @@ function LiveDetector(): React.ReactElement | null {
       setError(errorMessage);
     }
 
-    return null;
+    if (!results) {
+      logger.logError('executeModel: model results are empty');
+      return null;
+    }
+
+    if (!Array.isArray(results)) {
+      logger.logError('executeModel: expected an array of Tensors, got one Tensor', {
+        results,
+      });
+      return null;
+    }
+
+    const DETECTIONS_NUM_INDEX = 2;
+    const DETECTIONS_CLASSES_INDEX = 5;
+    const DETECTIONS_BOXES_INDEX = 0;
+    const DETECTIONS_SCORES_INDEX = 6;
+
+    const detectionsNum: number = tf.util.flatten(
+      results[DETECTIONS_NUM_INDEX].arraySync(),
+    )[0];
+
+    const detectionClasses: number[] = await tf.broadcastTo<tf.Rank.R1>(
+      tf.squeeze(results[DETECTIONS_CLASSES_INDEX]),
+      [detectionsNum],
+    ).array();
+
+    const detectionScores: number[] = await tf.broadcastTo<tf.Rank.R1>(
+      tf.squeeze(results[DETECTIONS_SCORES_INDEX]),
+      [detectionsNum],
+    ).array();
+
+    const detectionBoxes: number[][] = await tf.broadcastTo<tf.Rank.R2>(
+      tf.squeeze(results[DETECTIONS_BOXES_INDEX]),
+      [detectionsNum, 4],
+    ).array();
+
+    const modelPredictions: ModelPredictions = {
+      detectionsNum,
+      detectionClasses,
+      detectionBoxes,
+      detectionScores,
+    };
+
+    logger.logDebug('executeModel: parsed results', modelPredictions);
+
+    return modelPredictions;
   };
 
   const onFrame = async (video: HTMLVideoElement): Promise<void> => {
-    const predictions = await executeModel(video);
+    const predictions: ModelPredictions | null = await executeModel(video);
   };
 
   return (
