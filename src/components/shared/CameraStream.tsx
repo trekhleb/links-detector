97a3677f9929dd33e15efb297e70fbe7ee5b0e1e
Oleksii Trekhleb <trehleb@gmail.com>
2020-09-30 18:49:14 +0200

    Do smth.

--- src/components/shared/CameraStream.tsx
+++ src/components/shared/CameraStream.tsx
@@ -136,7 +136,7 @@ function CameraStream(props: CameraStreamProps): React.ReactElement {
     objectFit: 'cover',
     width: `${width}px`,
     height: `${height}px`,
-    filter: 'grayscale(100%)',
+    // filter: 'grayscale(100%)',
   };
 
   return (
