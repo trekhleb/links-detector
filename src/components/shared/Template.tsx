e9d100d010450c64ef48c39c005be032d65e24b5
Oleksii Trekhleb <trehleb@gmail.com>
2020-10-01 19:02:10 +0200

    Do smth.

--- src/components/shared/Template.tsx
+++ src/components/shared/Template.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import MainNavigation from './MainNavigation';
 
 type TemplateProps = {
   children?: React.ReactNode,
@@ -9,11 +8,6 @@ function Template(props: TemplateProps): React.ReactElement {
   const { children } = props;
   return (
     <main>
-      <header>
-        <nav>
-          <MainNavigation />
-        </nav>
-      </header>
       <section>
         {children}
       </section>
