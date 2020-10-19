02c0aab441493d40b01d362f87e496d8089e6ab1
Oleksii Trekhleb <trehleb@gmail.com>
2020-10-19 17:13:46 +0200

    Do smth.

--- src/utils/tesseract.ts
+++ src/utils/tesseract.ts
@@ -3,23 +3,102 @@ import {
 } from 'tesseract.js';
 import { buildLoggers } from './logger';
 
-type InitSchedulerProps = {
+export type InitSchedulerProps = {
   workersNum: number,
   language: string,
+  onError?: (error: any) => void,
+  onLoading?: (progress: number) => void, // [0, 1]
 };
 
-const initScheduler = async (props: InitSchedulerProps): Promise<Scheduler> => {
-  const { workersNum, language } = props;
+export enum WorkerLoadingStatuses {
+  LoadingCore = 'loading tesseract core',
+  Initializing = 'initializing tesseract',
+  Initialized = 'initialized tesseract',
+  LoadingLanguageTrainData = 'loading language traineddata',
+  LoadingLanguageTrainDataCached = 'loading language traineddata (from cache)',
+  LoadedLanguageTrainData = 'loaded language traineddata',
+  InitializingAPI = 'initializing api',
+  InitializedAPI = 'initialized api',
+}
+
+export type LoadingStages = WorkerLoadingStatuses[][];
+
+const loadingStages: LoadingStages = [
+  [
+    WorkerLoadingStatuses.LoadingCore,
+  ],
+  [
+    WorkerLoadingStatuses.Initializing,
+    WorkerLoadingStatuses.Initialized,
+  ],
+  [
+    WorkerLoadingStatuses.LoadingLanguageTrainData,
+    WorkerLoadingStatuses.LoadingLanguageTrainDataCached,
+    WorkerLoadingStatuses.LoadedLanguageTrainData,
+  ],
+  [
+    WorkerLoadingStatuses.InitializingAPI,
+    WorkerLoadingStatuses.InitializedAPI,
+  ],
+];
+
+export type WorkerLogEvent = {
+  status: WorkerLoadingStatuses,
+  workerId?: string,
+  progress?: number, // [0, 1]
+};
+
+const getLoadingProgressFromLog = (
+  scheduler: Scheduler,
+  logEvent: WorkerLogEvent,
+): number | null => {
+  if (!logEvent || !logEvent.status) {
+    return null;
+  }
+  const eventStageIndex: number = loadingStages.findIndex(
+    (stageStatuses: WorkerLoadingStatuses[]) => stageStatuses.includes(logEvent.status),
+  );
+  if (eventStageIndex === -1) {
+    return null;
+  }
+  const totalStagesNum: number = loadingStages.length;
+  const currentStageProgress: number = typeof logEvent.progress === 'number' ? logEvent.progress : 0;
+  return eventStageIndex / totalStagesNum + currentStageProgress / totalStagesNum;
+};
+
+export const initScheduler = async (props: InitSchedulerProps): Promise<Scheduler> => {
+  const {
+    workersNum,
+    language,
+    onError = (): void => {},
+    onLoading = (): void => {},
+  } = props;
 
   const logger = buildLoggers({ context: 'initScheduler' });
-  logger.logDebug('initScheduler');
+  logger.logDebug('initScheduler', { ...props });
+
+  const workerIDs: string[] = [];
 
   const scheduler: Scheduler = createScheduler();
 
+  const onWorkerLog = (logEvent: WorkerLogEvent): void => {
+    const progress: number | null = getLoadingProgressFromLog(scheduler, logEvent);
+    logger.logDebug('worker log', {
+      ...logEvent,
+      calcProgress: progress,
+      loadedWorkersNum: scheduler.getNumWorkers(),
+    });
+    onLoading(progress || 0);
+  };
+
+  const onWorkerError = (error: any): void => {
+    logger.logError('worker error', { ...error });
+    onError(error);
+  };
+
   const workerOptions: Partial<WorkerOptions> = {
-    logger: (arg: any): void => {
-      logger.logDebug('worker', arg);
-    },
+    logger: onWorkerLog,
+    errorHandler: onWorkerError,
   };
 
   const initWorker = async (): Promise<Worker> => {
@@ -38,10 +117,12 @@ const initScheduler = async (props: InitSchedulerProps): Promise<Scheduler> => {
     workers = await Promise.all(workersPromises);
   } catch (error) {
     logger.logError('Cannot init workers', { error });
+    onError(error);
   }
 
   workers.forEach((worker: Worker) => {
-    scheduler.addWorker(worker);
+    const workerID = scheduler.addWorker(worker);
+    logger.logDebug('addWorker', { workerID });
   });
 
   return scheduler;
