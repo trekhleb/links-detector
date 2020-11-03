1b147c8334fec945d6efa9ba0ec50abf2cf78157
Oleksii Trekhleb <trehleb@gmail.com>
2020-11-03 07:18:12 +0100

    Do smth.

--- src/components/screens/HomeScreen.tsx
+++ src/components/screens/HomeScreen.tsx
@@ -1,12 +1,22 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
+import { History } from 'history';
 
 import { ROUTES } from '../../constants/routes';
+import LaunchButton from '../shared/LaunchButton';
 
 function HomeScreen(): React.ReactElement {
+  const history: History = useHistory();
+
+  const onLaunch = (): void => {
+    history.push(ROUTES.detector.path);
+  };
+
   return (
     <div className="flex justify-center items-center flex-col">
-      <NavLink to={ROUTES.detector.path}>Detect Printed Links</NavLink>
+      <LaunchButton onClick={onLaunch}>
+        Scan
+      </LaunchButton>
     </div>
   );
 }
---
+++ src/components/shared/LaunchButton.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+type LaunchButtonProps = {
+  onClick: () => void,
+  children: React.ReactNode,
+};
+
+function LaunchButton(props: LaunchButtonProps): React.ReactElement {
+  const { children, onClick } = props;
+
+  return (
+    <button
+      onClick={onClick}
+      type="button"
+      className="border-0 rounded-full text-black bg-white w-48 h-48"
+    >
+      { children }
+    </button>
+  );
+}
+
+export default LaunchButton;
