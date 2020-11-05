02967b88d8b1f2b1bf2ec67cfe98772fa9e579a3
Oleksii Trekhleb <trehleb@gmail.com>
2020-11-05 19:50:54 +0100

    Do smth.

--- src/components/shared/LaunchButton.tsx
+++ src/components/shared/LaunchButton.tsx
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { CSSProperties } from 'react';
+
+import detectionImage from '../../images/detection.gif';
 
 type LaunchButtonProps = {
   onClick: () => void,
@@ -8,13 +10,28 @@ type LaunchButtonProps = {
 function LaunchButton(props: LaunchButtonProps): React.ReactElement {
   const { children, onClick } = props;
 
+  const buttonStyles: CSSProperties = {
+    backgroundImage: `url(${detectionImage})`,
+    backgroundSize: 'cover',
+  };
+
+  const buttonTextStyles: CSSProperties = {
+    backgroundColor: 'rgba(255, 255, 255, .8)',
+  };
+
   return (
     <button
+      style={buttonStyles}
       onClick={onClick}
       type="button"
-      className="border-0 rounded-full text-black bg-white w-48 h-48 text-4xl"
+      className="border-0 rounded-full text-black bg-white w-48 h-48 flex flex-row items-center justify-center"
     >
-      { children }
+      <div
+        style={buttonTextStyles}
+        className="font-light text-5xl rounded-full p-6 w-48 h-48 flex flex-row items-center justify-center"
+      >
+        { children }
+      </div>
     </button>
   );
 }
