3256213acf73c2ea040fa49f403cbedfc4d562e2
Oleksii Trekhleb <trehleb@gmail.com>
2020-11-05 20:37:02 +0100

    Do smth.

--- src/constants/style.ts
+++ src/constants/style.ts
@@ -1,5 +1,5 @@
 export const DETECTION_FOREGROUND_COLOR: string = '#000000';
-export const DETECTION_BACKGROUND_COLOR: string = '#ffff00';
+export const DETECTION_BACKGROUND_COLOR: string = '#f6e05e';
 
 // @see: https://tailwindcss.com/docs/padding#app
 export const FRAME_PADDING_CLASS: string = 'p-5';
