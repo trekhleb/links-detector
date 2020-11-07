0e5053c5c0022572181cc3c236c1e2999d0ed5f9
Oleksii Trekhleb <trehleb@gmail.com>
2020-11-07 08:50:03 +0100

    Do smth.

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -4,12 +4,15 @@ import { BrowserRouter } from 'react-router-dom';
 import Template from './shared/Template';
 import Routes from './Routes';
 import { BASE_APP_PATH } from '../constants/routes';
+import ErrorBoundary from './shared/ErrorBoundary';
 
 function App(): React.ReactElement {
   return (
     <BrowserRouter basename={BASE_APP_PATH}>
       <Template>
-        <Routes />
+        <ErrorBoundary>
+          <Routes />
+        </ErrorBoundary>
       </Template>
     </BrowserRouter>
   );
