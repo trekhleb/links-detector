ff9a236989d17a0a3b161b0fe11d2007f2531d6b
Oleksii Trekhleb <trehleb@gmail.com>
2020-11-07 08:58:02 +0100

    Do smth.

--- src/constants/routes.ts
+++ src/constants/routes.ts
@@ -1,4 +1,6 @@
-export const BASE_APP_PATH: string = '/';
+import { APP_TITLE, APP_TITLE_SEPARATOR } from './page';
+
+export const BASE_APP_PATH: string = '/links-detector';
 
 export const DEBUG_GET_PARAM = 'debug';
 
@@ -10,21 +12,29 @@ export enum RouteNames {
 
 type RouteType = {
   path: string,
+  title: string,
 };
 
 type RoutesType = {
   [routeName in RouteNames]: RouteType;
 };
 
+const generateAppTitle = (pageTitle: string): string => {
+  return `${APP_TITLE}${APP_TITLE_SEPARATOR}${pageTitle}`;
+};
+
 export const ROUTES: RoutesType = {
   [RouteNames.home]: {
     path: '/',
+    title: generateAppTitle('Home'),
   },
   [RouteNames.detector]: {
     path: '/detector',
+    title: generateAppTitle('Detection'),
   },
   [RouteNames.debug]: {
     path: '/debug',
+    title: generateAppTitle('Debug'),
   },
 };
 
