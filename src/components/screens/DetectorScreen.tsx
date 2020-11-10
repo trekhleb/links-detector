56f3735fffa68b286428b7b636d5e803a55fd765
Oleksii Trekhleb <trehleb@gmail.com>
2020-11-10 07:45:38 +0100

    Do smth.

--- src/components/screens/DetectorScreen.tsx
+++ src/components/screens/DetectorScreen.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { useHistory } from 'react-router-dom';
-import { History, LocationDescriptor } from 'history';
+import { useHistory, useLocation } from 'react-router-dom';
+import { History, Location, LocationDescriptor } from 'history';
 
 import LinksDetector from '../elements/LinksDetector';
 import Modal from '../shared/Modal';
@@ -9,10 +9,13 @@ import PageTitle from '../shared/PageTitle';
 
 function DetectorScreen(): React.ReactElement {
   const history: History = useHistory();
+  const location: Location = useLocation();
 
   const onModalClose = (): void => {
     const path: LocationDescriptor = {
       pathname: ROUTES[RouteNames.home].path,
+      search: location.search,
+      hash: location.hash,
     };
     history.push(path);
   };
