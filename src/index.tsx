4f0801d7367b3bb40682b9cbac643a09a91dc0de
Oleksii Trekhleb <trehleb@gmail.com>
2020-11-16 18:19:15 +0100

    Do smth.

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom';
 
 import './styles/tailwind.css';
 import App from './components/App';
-import * as serviceWorker from './serviceWorker';
+import * as serviceWorker from './serviceWorkerRegistration';
+import { PWA_ENABLED } from './configs/pwa';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -12,7 +13,8 @@ ReactDOM.render(
   document.getElementById('root'),
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+if (PWA_ENABLED) {
+  serviceWorker.register();
+} else {
+  serviceWorker.unregister();
+}
